867a9910395e6b1267cfce29adebfb555dfdc56f
Nicolas <nicolas.bandeira136@gmail.com>
2024-11-13 22:17:49 -0400

    AtividadeVeiculo

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿//try
+//{
+//    Veiculo v1 = new Veiculo();
+
+//    v1.SetNiv("0561561");
+//    v1.modelo = "Saveiro G6";
+//    v1.placa = "QBE-8324";
+
+//    Console.WriteLine(v1.getNiv());
+
+//} catch (Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+
+try
+{
+    Cliente c1 = new Cliente();
+
+    c1.setCpf("123.459.789-10");
+
+    Console.WriteLine(c1.getCpf());
+
+}catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
---
+++ Veiculo.cs
@@ -0,0 +1,52 @@
+﻿public class Veiculo
+{
+    private string _niv;
+    public string modelo;
+    public string placa;
+
+    public void SetNiv(string niv)
+    {
+        
+            if (niv.Length == 17)
+            {
+                _niv = niv;
+            }else
+            {
+                throw new Exception("O NIV deve conter 17 caracteres");
+            }
+        
+    }
+
+    public string getNiv()
+    {
+        return _niv;
+    }
+}
+
+public class Cliente
+{
+    public string _nome;
+    private string _cpf;
+    public string _sexo;
+    public DateOnly _datanascimento;
+
+    public void setCpf(string cpf)
+    {
+        cpf = cpf.Replace(".", "").Replace("-", "");        
+
+        if (cpf.Length == 11)
+        {
+            _cpf = cpf;
+        }
+        else
+        {
+             throw new Exception("O CPF deve conter 11 caracteres!");
+        }
+    }
+
+    public string getCpf()
+    {
+        return _cpf;
+    }
+}
+
